24c8616ddc33c6632e0ff5e4ee32eceffeb977da
Finnley Grange <dev.finnleygrange@icloud.com>
2024-02-20 09:00:29 +0000

    Jobs + Roles tables

---
+++ database/migrations/2024_02_19_191304_create_jobs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_title');
+            $table->string('post_number');
+            $table->decimal('pay_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
