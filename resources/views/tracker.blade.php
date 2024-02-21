c1107eb15315aa03c6b62e0e99f4a5c17fc8dc13
Finnley <dev.finnleygrange@icloud.com>
2024-02-21 11:44:27 +0000

    started creating tables on website

--- resources/views/tracker.blade.php
+++ resources/views/tracker.blade.php
@@ -10,8 +10,6 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="relative flex flex-col w-full h-full overflow-scroll shadow-md rounded-xl bg-clip-border text-gray-900 dark:text-gray-100 p-6">
                     <table class="w-full text-left table-auto min-w-max">
-
-                        @foreach ($array as $key=>$value)
                         <tr>
                             <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
                                 <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
@@ -19,7 +17,6 @@
                                 </p>
                             </th>
                         </tr>
-                        @endforeach
                         <tr>
                             <td class="p-4 border-b border-blue-gray-50">
                                 <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
