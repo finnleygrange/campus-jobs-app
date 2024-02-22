cf8486589e073d251b0e893c067459f4eda4b80b
Finnley <dev.finnleygrange@icloud.com>
2024-02-22 12:52:13 +0000

    adding maatwebsite/excel package

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -57,7 +57,6 @@
                             </td>
                         </tr>
                         @endforeach
-                        
                     </table>
                 </div>
             </div>
