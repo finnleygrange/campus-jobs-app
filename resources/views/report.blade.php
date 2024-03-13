d5181caa90f00352d772cc4744dac7a221bac8cc
DaianaPopa <116299749+DaianaPopa@users.noreply.github.com>
2024-03-13 21:53:10 +0000

    made the report section, but i need help with some controller error :)))

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Reports') }}
+            {{ __('Report') }}
         </h2>
     </x-slot>
 
@@ -9,8 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("This is the reports page.") }}
-
                     <table class="w-full text-left table-auto min-w-max">
                         <tr>
                             <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
@@ -33,7 +31,6 @@
                                     Job Title
                                 </p>
                             </th>
-                        @foreach ($reports as $report)
                             <tr>
                             <td class="p-4 border-b border-blue-gray-50">
                                 <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
@@ -56,9 +53,7 @@
                                 </p>
                             </td>
                         </tr>
-                        @endforeach
                     </table>
-    
                 </div>
             </div>
         </div>
--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -1,31 +1,23 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Reports') }}
+            {{ __('Jobs') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("This is the reports page.") }}
-
+                    <div class="py-6">
+                        <a href="{{ route('jobs.create') }}" class="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 my-2 rounded">
+                            Create New Job
+                        </a>
+                    </div>
                     <table class="w-full text-left table-auto min-w-max">
                         <tr>
                             <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
                                 <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                                    Date
-                                </p>
-                            </th>
-                            <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                                <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                                    Line Manager Name
-                                </p>
-                            </th>
-                            <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                                <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                                    Requested Hours
+                                    Job ID
                                 </p>
                             </th>
                             <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
@@ -33,32 +25,48 @@
                                     Job Title
                                 </p>
                             </th>
-                        @foreach ($reports as $report)
-                            <tr>
+                            <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                    Job Description
+                                </p>
+                            </th>
+                            <th class="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
+                                <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
+                                    Actions
+                                </p>
+                            </th>
+                        </tr>
+                        @foreach ($jobs as $job)
+                        <tr>
                             <td class="p-4 border-b border-blue-gray-50">
                                 <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                                    {{ $reports->date}}
+                                    {{ $job->id }}
                                 </p>
                             </td>
                             <td class="p-4 border-b border-blue-gray-50">
                                 <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                                    {{ $reports->manager_name}}
+                                    {{ $job->job_title }}
                                 </p>
                             </td>
                             <td class="p-4 border-b border-blue-gray-50">
                                 <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                                    {{ $reports->hours_requested}}
+                                    {{ $job->job_description }}
                                 </p>
                             </td>
                             <td class="p-4 border-b border-blue-gray-50">
-                                <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                                    {{ $reports->job_title}}
-                                </p>
+                                <div class="flex">
+                                    <a href="{{ route('jobs.show', $job->id) }}" class="mr-2 text-indigo-600 hover:text-indigo-900">Show</a>
+                                    <a href="{{ route('jobs.edit', $job->id) }}" class="mr-2 text-indigo-600 hover:text-indigo-900">Edit</a>
+                                    <form action="{{ route('jobs.destroy', $job->id) }}" method="POST" class="inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-red-600 hover:text-red-900">Delete</button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                         @endforeach
                     </table>
-    
                 </div>
             </div>
         </div>
