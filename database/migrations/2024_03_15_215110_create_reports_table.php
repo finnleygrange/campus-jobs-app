ff3903f34dd04a0b351b1cc09896a5e83980af8d
Finnley <dev.finnleygrange@icloud.com>
2024-03-18 13:59:52 +0000

    fixed creating reports, removed date column

--- database/migrations/2024_03_15_215110_create_reports_table.php
+++ database/migrations/2024_03_15_215110_create_reports_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
-            $table->date('date');
             $table->string('job_title');
             $table->string('manager_name');
             $table->integer('hours_requested');
