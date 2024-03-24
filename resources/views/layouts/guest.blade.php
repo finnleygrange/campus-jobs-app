aaa834e2639b36922821fcd2f0f4038c36c9cbdd
Finnley Grange <dev.finnleygrange@icloud.com>
2024-03-24 03:14:57 +0000

    fixed overly larged logos on login/register page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
+            <div class="w-64">
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
