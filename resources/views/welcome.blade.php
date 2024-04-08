ad27cd9bd643bb1cf7720da8f7dd97a01f75e1ff
Finnley Grange <dev.finnleygrange@icloud.com>
2024-04-08 07:23:09 +0100

    fixed dashboard button on homescreen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,11 +15,11 @@
     @if (Route::has('login'))
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
             @auth
-                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-200 hover:text-gray-300 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 px-4 py-2 rounded shadow">Dashboard</a>
+                <a href="{{ url('/dashboard') }}" class="bg-custom2 font-semibold text-white hover:text-gray-300 dark:text-gray-400 dark:hover:text-white focus:outline-none focus:ring focus:text-white focus:bg-gray-600 px-4 py-2 rounded shadow">Dashboard</a>
             @else
-                <a href="{{ route('login') }}" class="bg-custom2 font-semibold text-white hover:text-gray-300 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 px-4 py-2 rounded shadow">Log in</a>
+                <a href="{{ route('login') }}" class="bg-custom2 font-semibold text-white hover:text-gray-300 dark:text-gray-400 dark:hover:text-white focus:outline-none focus:ring focus:text-white focus:bg-gray-600 px-4 py-2 rounded shadow">Log in</a>
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="bg-custom2 font-semibold text-white hover:text-gray-300 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 px-4 py-2 rounded shadow ml-4">Register</a>
+                    <a href="{{ route('register') }}" class="bg-custom2 font-semibold text-white hover:text-gray-300 dark:text-gray-400 dark:hover:text-white focus:outline-none focus:ring focus:text-white focus:bg-gray-600 px-4 py-2 rounded shadow ml-4">Register</a>
                 @endif
             @endauth
         </div>
